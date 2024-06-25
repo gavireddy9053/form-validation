3bb0b40142187f06bc76076981d6a1cd6fb449e6
Gavireddy Linitha <gavireddy9053@gmail.com>
2024-06-25 22:25:59 +0530

    updated css

--- src/App.js
+++ src/App.js
@@ -1,32 +1,56 @@
 import './App.css';
-import {useState} from 'react'
+import {useState} from 'react';
 
 function App() {
-  const [data,setData] = useState({
-    name:'',
-    password:''
-  })
-  const MyEvent = (event)=>{
-    setData({...data,[event.target.name]:event.target.value})
-  }
-  const handleSubmit = (e)=>{
-    e.preventDefault()
-    console.log(data)
-  }
+  const initialData = {
+    name: '',
+    password: ''
+  };
+
+  const [data, setData] = useState(initialData);
+
+  const MyEvent = (event) => {
+    setData({...data, [event.target.name]: event.target.value});
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(data);
+  };
+
+  const handleReset = () => {
+    setData(initialData);
+  };
+
   return (
     <div className="App">
-      <h1>SignIn</h1>
-      <div>
-        <label>UserName</label>
-        <input type='text' name='name' placeholder='Enter your username' onChange={MyEvent}/>
-       </div>
-       <div>
-        <label>Password</label>
-        <input type='password' password='password' placeholder='Enter password'  onChange={MyEvent}/>
-       </div>
-       <div>
-      <button onClick={handleSubmit}>sumbit</button>
-       </div>
+      <h1>Sign In</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>UserName</label>
+          <input 
+            type="text" 
+            name="name" 
+            placeholder="Enter your username" 
+            value={data.name}
+            onChange={MyEvent} 
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <input 
+            type="password" 
+            name="password" 
+            placeholder="Enter password" 
+            value={data.password}
+            onChange={MyEvent} 
+          />
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+          <button type="button" onClick={handleReset}>Reset</button>
+        </div>
+      </form>
     </div>
   );
 }
